7b8ff271f745d6429b81fd4d30cdcc26eb4ce488
NovikovaValeriya <valeriya_novikova_06@mail.ru>
2024-09-29 18:40:48 +0500

    Task7

--- Tyuiu.NovikovaVA.Sprint1.Task7.V27.Lib/DataService.cs
+++ Tyuiu.NovikovaVA.Sprint1.Task7.V27.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.NovikovaVA.Sprint1.Task7.V27.Lib
     {
         public double Calculate(double x, double y)
         {
-            double res = (Math.Pow(Math.Cos(x), 2) + Math.Pow(Math.Sin(y), 2)) / (Math.Sin(y) + 1) - (x * y - 12) / (15 + Math.Cos(x));
+            double res = (((Math.Cos(Math.Pow(x, 2)) + Math.Sin(Math.Pow(y, 2)))) / (Math.Sin(y) + 1)) - ((x * y - 12) / (15 + Math.Cos(x)));
             return Math.Round(res,3);
         }
     }
